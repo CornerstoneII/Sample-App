048b88a2066b63e8bdadc7be891ec5139530537f
CornerstoneII <seunmyles@gmail.com>
2020-12-23 18:19:50 +0100

    Add direct test of the full_title helper

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,5 +1,5 @@
 require 'test_helper'
-  class SiteLayoutTest < ActionDispatch::IntegrationTest
+class SiteLayoutTest < ActionDispatch::IntegrationTest
     test "layout links" do
       get root_path
       assert_template 'static_pages/home'
@@ -7,5 +7,8 @@ require 'test_helper'
       assert_select "a[href=?]", help_path
       assert_select "a[href=?]", about_path
       assert_select "a[href=?]", contact_path
+
+      get contact_path
+      assert_select "title", full_title("Contact")
     end
-  end
+end
