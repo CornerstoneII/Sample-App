5ba0dd91613e616c5c5e89892e4be50effa77c22
CornerstoneII <seunmyles@gmail.com>
2020-12-28 12:01:35 +0100

    Update the Sessions controller test to use the login route

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -6,4 +6,9 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get new" do
+    get login_path
+    assert_response :success
+  end
+
 end
