432b2ce961c19d13ecd919f4fa9cc5d348a90f95
CornerstoneII <seunmyles@gmail.com>
2020-12-31 14:57:24 +0100

    Testing that edit and update are protected from attacks

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -13,7 +13,8 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
-    patch user_path(@user), params: { user: { name: '', email: 'foo@invalid', password: 'foo', password_confirmation: 'bar' } }
+    patch user_path(@user), params: { user: { name: '', email: 'foo@invalid',
+                                              password: 'foo', password_confirmation: 'bar' } }
     assert_template 'users/edit'
     assert_select 'div.alert', text: 'The form contains 4 errors.'
   end
