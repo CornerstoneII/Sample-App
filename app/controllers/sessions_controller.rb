0cda2b34c501313145c0ba29bfb8f17ce3aa446c
CornerstoneII <seunmyles@gmail.com>
2020-12-31 17:03:08 +0100

    Add a Sessions create action with friendly forwarding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,24 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
       log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
+      redirect_back_or @user
     else
-      flash.now[:danger] = 'Invalid email/password combination' #Not quite right!
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
     end
   end
 
   def current_user
-    if session[:user_id]
-      User.find_by(id: session[:user_id])
-    end
+    User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   def destroy
     log_out if logged_in?
     redirect_to root_url
   end
- end
+end
