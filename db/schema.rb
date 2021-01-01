ea90cd781db749753b09b4c5ca80a6b0c3e50962
CornerstoneII <seunmyles@gmail.com>
2021-01-01 13:19:18 +0100

    Run rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_120503) do
+ActiveRecord::Schema.define(version: 2021_01_01_120847) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_12_30_120503) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
 end
